30fc8f7898217f5b3e424f88755ffdfa11837fd1
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-08-16 23:39:33 -0300

    feat: banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,35 +2,40 @@
 import './styles.css';
 
 // React Hooks
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 // React Component
 import { Banner } from './Utils/Banner';
 
-// React Icons
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
-
 export const BannerContainer = () => {
     const carousel = useRef<HTMLDivElement>(null)
 
-    const handleLeftClick = () => {
-        carousel && (carousel.current!.scrollLeft -= carousel.current!.offsetWidth + 24)
+    const [ classActive, setClassActive ] = useState('')
+
+    const handleClickIndexImage = (index) => {
+        const indexedImage = index * carousel.current!.offsetWidth
+        console.log(indexedImage)
+        carousel && (carousel.current!.scrollLeft = indexedImage)
+        setClassActive("active")
     }
 
-    const handleRightClick = () => {
-        carousel && (carousel.current!.scrollLeft += carousel.current!.offsetWidth + 24)
-    }
+    const linkImages = ["https://casamobilia.simply.app.br/arquivo_back/9155/banner/5399d3b4e1c769b23e4caa9f5b0917f820230721174635.webp", "https://casamobilia.simply.app.br/arquivo_back/9155/banner/fb3118396607bf3d70271ee61a5b6dd720230721174610.webp", "https://casamobilia.simply.app.br/arquivo_back/9155/banner/fb3118396607bf3d70271ee61a5b6dd720230721174610.webp"]
 
     return (
         <div className="principal-container__banner" >
             <div className="banners" ref={ carousel }>
-                <Banner image="https://casamobilia.simply.app.br/arquivo_back/9155/banner/5399d3b4e1c769b23e4caa9f5b0917f820230721174635.webp"/>
-
-                <Banner image="https://casamobilia.simply.app.br/arquivo_back/9155/banner/fb3118396607bf3d70271ee61a5b6dd720230721174610.webp"/>
+                {
+                    linkImages && linkImages.map((imageLink, index) => {
+                        return <Banner image={imageLink} key={index}/>
+                    })
+                }
             </div>
             <div className="button-product__banner">
-                <button onClick={ handleLeftClick }><AiOutlineArrowLeft /></button>
-                <button onClick={ handleRightClick }><AiOutlineArrowRight  /></button>
+                {
+                    linkImages.map((item, index) => {
+                        return <div className={`dot-banner ${item}`} onClick={ () => handleClickIndexImage(index) }></div>
+                    })
+                }
             </div>
         </div>
     )
